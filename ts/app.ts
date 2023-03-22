aec1f1483b609479440dcd4ca28f1ba527d5128a
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-03-22 14:03:33 +0100

    fixed app.ts

--- ts/app.ts
+++ ts/app.ts
@@ -1,8 +1,6 @@
 "use strict";
-import { fetcher } from "./fetcher";
 import news from "./news";
 import printCategories from "./categories";
 
 document.addEventListener("DOMContentLoaded", () => news());
-const baseUrl = "http://46.101.130.27/wp-json/";
 printCategories();
