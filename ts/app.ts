f48cc672d861366659c30f2c873673bd3121dc6b
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-03-22 13:49:10 +0100

    fixed scss for news

--- ts/app.ts
+++ ts/app.ts
@@ -1,2 +1,7 @@
-'use strict'
-import fetcher from "./fetcher";
+"use strict";
+
+import { fetcher } from "./fetcher";
+import news from "./news";
+
+document.addEventListener("DOMContentLoaded", () => news());
+const baseUrl = "http://46.101.130.27/wp-json/";
