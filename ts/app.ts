d14907b10f72ace42fdf319c55a635a8869f8fea
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-03-22 12:37:30 +0100

    Print menu items

--- ts/app.ts
+++ ts/app.ts
@@ -5,6 +5,15 @@ const storeName = document.querySelector("h1.storeName") as HTMLHeadingElement;
 let dataName = "";
 async function init() {
   const storeData = await fetcher("");
+  const menuItems = await fetcher("/wp/v2/menu-items/19");
+  const ul = document.createElement("ul");
+  menuItems.map((item: { title: string }) => {
+    console.log(item);
+    const li = document.createElement("li");
+    li.innerText = item.title;
+    ul.appendChild(li);
+  });
+  menu.appendChild(ul);
   dataName = storeData.name;
   storeName.innerHTML = dataName;
 }
