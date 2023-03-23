4f094aeb971bbae5935904d6f76da9b3ddce1b1f
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-03-23 14:58:15 +0100

    print done and start of styling

--- ts/app.ts
+++ ts/app.ts
@@ -2,5 +2,7 @@
 import news from "./news";
 import printCategories from "./categories";
 import printHeader from "./header";
+import printLandingPage from "./landing-page";
 
 printHeader();
+printLandingPage();
