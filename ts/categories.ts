99003c1e8fcb12ad299af33b1d165e56aaf69b0a
Anton Jonsson <94antjon@skola.boras.se>
2023-03-24 10:51:50 +0100

    add print selected product category function

--- ts/categories.ts
+++ ts/categories.ts
@@ -1,25 +1,43 @@
 import fetcher from "./fetcher";
+import printProducts from "./products";
+const productsUrl = "/wc/v3/products";
+const contentArea = document.querySelector("main") as HTMLElement;
+const productCategoryUrl = "/wc/v3/products/categories";
 
 export default function printCategories() {
-    const contentArea = document.querySelector("main") as HTMLElement;
-    const productCategoryUrl = "/wc/v3/products/categories/";
     fetcher(productCategoryUrl).then((data) => {
         const categories = data;
 
         for (let i = 0; i < categories.length; i++) {
-            const categoryCard = document.createElement(
-                "div"
-            ) as HTMLDivElement;
-            categoryCard.setAttribute("class", "categoryCard");
-            categoryCard.innerText = categories[i].name;
             if (categories[i].name === "Uncategorized") {
                 continue;
             }
+            const productcategoryCard = document.createElement(
+                "div"
+            ) as HTMLDivElement;
+            productcategoryCard.setAttribute("id", categories[i].name);
+            productcategoryCard.setAttribute("class", "categoryCard");
+            productcategoryCard.innerText = categories[i].name;
 
-            categoryCard.addEventListener("click", () => {
-                console.log(categories[i].name);
+            productcategoryCard.addEventListener("click", (event) => {
+                fetcher(productsUrl).then((data) => {
+                    const products = data; //typa denna sen
+                    const target = event.target as HTMLDivElement;
+                    console.log("target", target.id);
+                    const productsInCategory: any = products.filter(
+                        (product: any) => {
+                            return product.categories.find((category: any) => {
+                                return target.id == category.name;
+                            });
+                        }
+                    );
+
+                    // console.log("filtrerade prod", productsInCategory);
+                    printProducts(productsInCategory);
+                });
             });
-            contentArea.appendChild(categoryCard);
+
+            contentArea.appendChild(productcategoryCard);
         }
     });
 }
