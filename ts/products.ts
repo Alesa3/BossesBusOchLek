17987165c997d72e9c371408e41d2cc072064380
Sn1cke <113979281+Sn1cke@users.noreply.github.com>
2023-03-27 09:19:11 +0200

    Add wrapper on products

--- ts/products.ts
+++ ts/products.ts
@@ -1,35 +1,37 @@
-import fetcher from "./fetcher";
-
 const contentArea = document.querySelector("main") as HTMLElement;
 
 interface product {
-    name: string;
-    permalink: string;
-    images: Array<{ src: string }>;
-    price: number;
+  name: string;
+  permalink: string;
+  images: Array<{ src: string }>;
+  price: number;
 }
 
 export default function printselected(selected: Array<product>) {
-    contentArea.innerHTML = "";
-    for (let i = 0; i < selected.length; i++) {
-        const productCard = document.createElement("div");
-        productCard.innerHTML = "";
-        productCard.setAttribute("class", "productCard");
-        const productTitle = document.createElement("a");
-        productTitle.innerText = selected[i].name;
-        productTitle.href = selected[i].permalink;
-        let productImage = document.createElement("img") as HTMLImageElement;
-        productImage.src = selected[i].images[0].src;
+  contentArea.innerHTML = "";
+  const productsWrapper = document.createElement("section");
+  productsWrapper.setAttribute("id", "products-wrapper");
+  contentArea.append(productsWrapper);
 
-        let productPrice = document.createElement("p");
+  for (let i = 0; i < selected.length; i++) {
+    const productCard = document.createElement("div");
+    productCard.innerHTML = "";
+    productCard.setAttribute("class", "productCard");
+    const productTitle = document.createElement("a");
+    productTitle.innerText = selected[i].name;
+    productTitle.href = selected[i].permalink;
+    let productImage = document.createElement("img") as HTMLImageElement;
+    productImage.src = selected[i].images[0].src;
 
-        productPrice.innerText = selected[i].price + "kr";
+    let productPrice = document.createElement("p");
 
-        productCard.addEventListener("click", () => {
-            console.log(selected[i].name);
-        });
-        contentArea.append(productCard);
-        productCard.append(productImage);
-        productCard.append(productTitle, productPrice);
-    }
+    productPrice.innerText = selected[i].price + "kr";
+
+    productCard.addEventListener("click", () => {
+      console.log(selected[i].name);
+    });
+    productsWrapper.append(productCard);
+    productCard.append(productImage);
+    productCard.append(productTitle, productPrice);
+  }
 }
