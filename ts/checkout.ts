46497ff7feddd602193f00d469fd41d4f68db555
Alexander Esbjörnsson <alex.esbj@gmail.com>
2023-03-30 10:41:58 +0200

    fixed post url and added form labels + cleanup

--- ts/checkout.ts
+++ ts/checkout.ts
@@ -23,35 +23,58 @@ export default function printCheckout() {
 
   const checkoutButton = document.createElement("button");
   checkoutButton.innerText = "Skicka order";
-  checkoutButton.addEventListener("click", postOrder);
   printForm();
+  checkoutButton.addEventListener("click", postOrder);
 
   main.append(productsWrapper, totalPriceEl, checkoutButton);
 }
 function printForm() {
   const form = document.createElement("form");
+  const firstNameLabel = document.createElement("label")
+  firstNameLabel.innerText = "Namn"
   const firstName = document.createElement("input");
   firstName.className="firstName"
   const lastName = document.createElement("input");
   lastName.className="lastName"
+  const lastNameLabel = document.createElement("label");
+  lastNameLabel.innerText ="Efternamn"
   const adress = document.createElement("input");
+  const adressLabel = document.createElement("label")
+  adressLabel.innerText= "Gata"
   adress.className="adress"
   const city = document.createElement("input");
+  const cityLabel = document.createElement("label");
+  cityLabel.innerText = "Ort"
   city.className="city"
   const postcode = document.createElement("input");
+  const postcodeLabel = document.createElement("label");
+  postcodeLabel.innerText = "Postnummer"
   postcode.className="postCode"
   const email = document.createElement("input");
+  const emailLabel = document.createElement("label");
+  emailLabel.innerText = "E-post"
   email.className="email"
+  email.setAttribute("type", "email")
   const phone = document.createElement("input");
+  const phoneLabel = document.createElement("label");
+  phoneLabel.innerText = "Telefon"
   phone.className="phone"
+  phone.setAttribute("type", "tel")
 
   form.append(
+    firstNameLabel,
     firstName,
+    lastNameLabel,
     lastName,
+    adressLabel,
     adress,
+    cityLabel,
     city,
+    postcodeLabel, 
     postcode,
+    emailLabel,
     email,
+    phoneLabel,
     phone
   );
   main.append(form)
@@ -91,6 +114,14 @@ function postOrder() {
   cartArr.map((item: any) => {
     item.id = Number(item.id)
   })
+
+  const firstName = document.querySelector("input.firstName") as HTMLInputElement
+  const lastName = document.querySelector("input.lastName") as HTMLInputElement
+  const adress =  document.querySelector("input.adress") as HTMLInputElement
+  const city = document.querySelector("input.city") as HTMLInputElement
+  const postCode = document.querySelector("input.postCode") as HTMLInputElement
+  const email = document.querySelector("input.email") as HTMLInputElement
+  const phone = document.querySelector("input.phone") as HTMLInputElement
   // SKAPA BODY
   const order = {
     payment_method: "bacs",
@@ -98,47 +129,46 @@ function postOrder() {
     set_paid: true,
     customer_id: 1,
     billing: {
-      first_name: document.querySelector("input.name"),
-      last_name: document.querySelector("input.lastName"),
-      adress_1: document.querySelector("input.adress"),
-      city: document.querySelector("input.city"),
-      postcode: document.querySelector("input.postCode"),
+      first_name: firstName.value,
+      last_name: lastName.value,
+      adress_1: adress.value,
+      city: city.value,
+      postcode: postCode.value,
       country: "SE",
-      email: document.querySelector("input.email"),
-      phone: document.querySelector("input.phone")
+      email: email.value,
+      phone: phone.value,
     },
     shipping: {
-      first_name: document.querySelector("input.name"),
-      last_name: document.querySelector("input.lastName"),
-      adress_1: document.querySelector("input.adress"),
-      city: document.querySelector("input.city"),
-      postcode: document.querySelector("input.postCode"),
+      first_name: firstName.value,
+      last_name: lastName.value,
+      adress_1: adress.value,
+      city: city.value,
+      postcode: postCode.value,
       country: "SE",
-      email: document.querySelector("input.email"),
-      phone: document.querySelector("input.phone")
+      email: email.value,
+      phone: phone.value,
     },
     line_items: cartArr,
     shipping_lines: [
       {
         method_id: "flat_rate",
         method_title: "Flat rate",
-        total: "100"
+        total: "0"
       }
     ]
   };
-
-  fetch("http://46.101.130.27/rest/wp-json/wc/v3/orders", {
+  console.log(order)
+  fetch("http://46.101.130.27/wp-json/wc/v3/orders", {
     method: "POST",
     headers: {
       "Content-type": "application/json",
     },
-    mode: "no-cors",
     body: JSON.stringify(order)
   })
     .then((res) => res.json())
     .then((data) => {
       console.log("Order skickad", data);
-      localStorage.setItem("cart", JSON.stringify([]));
+      // localStorage.setItem("cart", JSON.stringify([]));
       printCheckout();
     })
     .catch((err) => console.log("err", err));
